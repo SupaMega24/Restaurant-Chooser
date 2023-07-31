1b04c4e7a6a19041d5b432a052d13c76dca74ad1
Charles Jones <victoriaeld24@gmail.com>
2023-07-31 15:13:47 +0900

    first commit - committing all files to the repository

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import random
+import time
+
+print("""
+Can't decide where to eat? 
+
+No worries, I'm here to help.
+""")
+
+print("Just type your choices to the quetion below and press enter😉\n")
+
+choice_string = input("Tell me some restaurants that you like separated by a comma.\n")
+choices = choice_string.split(", ")
+
+menu = random.choice(choices).title()
+
+print("\n💭...")
+time.sleep(2)
+print("\nWe are going to eat at " + menu.upper() + "!")
